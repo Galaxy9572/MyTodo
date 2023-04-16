3c4a994466ba27bc138426888e4e5751826e650b
JY <ljy565880512@qq.com>
2023-04-16 13:36:14 +0800

    Initial Commit

---
+++ MyTodo/MyTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodoApp.swift
+//  MyTodo
+//
+//  Created by 廖俊瑶 on 2023/4/16.
+//
+
+import SwiftUI
+
+@main
+struct MyTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
